5b30af0f7f07e35535f303d6439445f80a3b37a1
luckinha <luckinha45@gmail.com>
2022-09-07 18:29:01 -0300

    aula 03

--- aula03/script.js
+++ aula03/script.js
@@ -1,71 +1,48 @@
+var max = 200;
+var tentativas = 10;
+var fim = false;
+
+var objetivo = Math.floor(Math.random() * (max+1));
+
 function getElement(id) {
-    return document.getElementById(id);
+	return document.getElementById(id);
 }
 
-function mudaConv() {
-    let e = getElement('slct_pagina').value;
-
-    switch (e) {
-        case 'moeda':
-            getElement('slct_moeda').hidden = false;
-            getElement('slct_dist').hidden = true;
-            getElement('slct_bitcoin').hidden = true;
-            getElement('slct_temp').hidden = true;
-            break;
-
-        case 'dist':
-            getElement('slct_moeda').hidden = true;
-            getElement('slct_dist').hidden = false;
-            getElement('slct_bitcoin').hidden = true;
-            getElement('slct_temp').hidden = true;
-            break;
-        
-        case 'bitcoin':
-            getElement('slct_moeda').hidden = true;
-            getElement('slct_dist').hidden = true;
-            getElement('slct_bitcoin').hidden = false;
-            getElement('slct_temp').hidden = true;
-            break;
-
-        case 'temp':
-            getElement('slct_moeda').hidden = true;
-            getElement('slct_dist').hidden = true;
-            getElement('slct_bitcoin').hidden = true;
-            getElement('slct_temp').hidden = false;
-            break;
-    }
+function changeRatio(value, oldMin, oldMax, newMin, newMax) {
+	return ((value - oldMin) * (newMax - newMin)) / (oldMax - oldMin) + newMin;
 }
 
-function ConverterDist() {
-    let anosLuz = getElement('valor_dist').value;
-    anosLuz = isNaN(anosLuz) ? 0 : anosLuz;
+function Chutar() {
+	if (!fim) {
+		let dica_atual = getElement("dica_atual");
+		let resultado = getElement('resultado');
+	
+		let guess = parseInt(getElement('valor').value);
+		guess = isNaN(guess) ? 0 : guess;
+	
+		if (guess < 0 || guess > max) {
+			resultado.innerHTML = 'Valor fora dos limites!';
+			return;
+		}
+		else {
+			let percent = (1 - Math.abs(guess - objetivo) / max)*100;
+			dica_atual.style.width = percent + '%';
+			getElement('porcentagem').innerHTML = percent.toFixed(2)+'%';
 
-    let km = anosLuz * 9.461e+12;
-    getElement('valor_result').innerHTML = km.toExponential() + ' km';
-}
+			if (guess == objetivo) {
+				fim = true;
+				resultado.style.color = 'green';
+				resultado.innerHTML = 'ENCONTROU EM ' + (10-tentativas) +' TENTATIVAS!';
+			}
+			else {
+				tentativas--;
+				resultado.innerHTML = "FALTAM " + tentativas + " TENTATIVAS.";
+				if (tentativas == 1) fim = true;
+			}
+		}
+	}
+	else if (fim && tentativas==1) {
+		resultado.innerHTML = 'ACABOU AS TENTATIVAS! PERDESTE :(';
+	}
 
-function converterTemp() {
-    let c = parseFloat(getElement('temp_celcius').value.replace(',', '.'));
-    let f = '';
-    let k = '';
-    if (!isNaN(c)) {
-        f = (c*1.8)+32;
-        k = c+273.15;
-    }
-
-    getElement('temp_fhrnt').innerHTML = f;
-    getElement('temp_kelvin').innerHTML = k;
-}
-
-async function ConverterMoeda() {
-    let c1 = getElement('select_currency1').value;
-    let c2 = getElement('select_currency2').value;
-    let valorC1 = getElement('valor').value;
-    valorC1 = isNaN(valorC1) ? 0 : valorC1;
-    
-    // Usa uma API para pegar o peso das moedas com relação à primeira moeda colocada
-    let result = await (await fetch(`https://api.exchangerate-api.com/v4/latest/${c1}`)).json();
-
-    let valorC2 = result.rates[c2] * valorC1;
-    getElement('valorConvertido').innerHTML = `${c2} ` + valorC2.toFixed(2);
 }
